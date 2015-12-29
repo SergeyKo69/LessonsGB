bbc908af210ed74e0dbada5bfe47d3ed0580e5d2
koguts <koguts@macbook-sergej.dlink>
2015-12-30 00:15:24 +0300

    2

--- Lesson7/src/Ex1.java
+++ Lesson7/src/Ex1.java
@@ -1,12 +1,17 @@
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 
 public class Ex1 extends JPanel{
 	public Ex1(){
 		JLabel lblTitle = new JLabel();
 		lblTitle.setText("Exercise №1");
+		
 		add(lblTitle,BorderLayout.CENTER);
+		JLabel lblDescr = new JLabel();
+		
 	}
 }
