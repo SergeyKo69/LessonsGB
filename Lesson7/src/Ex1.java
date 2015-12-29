331f9d75ecd1e911dfc2a20aff750da86c3eb70a
koguts <koguts@macbook-sergej.dlink>
2015-12-30 00:04:16 +0300

    1

--- Lesson7/src/Ex1.java
+++ Lesson7/src/Ex1.java
@@ -1,5 +1,12 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Ex1 extends JPanel{
-	
+	public Ex1(){
+		JLabel lblTitle = new JLabel();
+		lblTitle.setText("Exercise №1");
+		add(lblTitle,BorderLayout.CENTER);
+	}
 }
