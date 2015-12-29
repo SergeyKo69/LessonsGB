461241fa97a0cfffb148ce64c715c2bf5ad03eac
koguts <koguts@macbook-sergej.dlink>
2015-12-29 23:47:44 +0300

    Begin

--- Lesson7/src/MainFrame.java
+++ Lesson7/src/MainFrame.java
@@ -7,10 +7,10 @@ public class MainFrame extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100,100,500,500);
 		JTabbedPane tabPane = new JTabbedPane();
-		JPanel panel1 = new JPanel(); // Ex. 1
 		JPanel panel2 = new JPanel(); // Ex. 2
-		tabPane.addTab("Ex1",panel1);
+		tabPane.addTab("Ex1",new Ex1());
 		tabPane.addTab("Ex2",panel2);
 		add(tabPane);
 	}
+	
 }
