8b60f2adfe0e063e5d25d69f04975ae0f261bd89
koguts <koguts@macbook-sergej.dlink>
2015-12-29 23:59:56 +0300

    create

--- Lesson7/src/MainFrame.java
+++ Lesson7/src/MainFrame.java
@@ -1,5 +1,4 @@
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class MainFrame extends JFrame{
@@ -7,9 +6,19 @@ public class MainFrame extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100,100,500,500);
 		JTabbedPane tabPane = new JTabbedPane();
-		JPanel panel2 = new JPanel(); // Ex. 2
 		tabPane.addTab("Ex1",new Ex1());
-		tabPane.addTab("Ex2",panel2);
+		tabPane.addTab("Ex2",new Ex2());
+		tabPane.addTab("Ex3",new Ex3());
+		tabPane.addTab("Ex4",new Ex4());
+		tabPane.addTab("Ex5",new Ex5());
+		tabPane.addTab("Ex6",new Ex6());
+		tabPane.addTab("Ex7",new Ex7());
+		tabPane.addTab("Ex8",new Ex8());
+		tabPane.addTab("Ex9",new Ex9());
+		tabPane.addTab("Ex10",new Ex10());
+		tabPane.addTab("Ex11",new Ex11());
+		tabPane.addTab("Ex12",new Ex12());
+		tabPane.addTab("Ex13",new Ex13());
 		add(tabPane);
 	}
 	
